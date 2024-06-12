8530d811db057eff88cce9f0243096f5b24db7bf
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-06-12 18:51:42 -0500

    primer commit del lab

---
+++ LAB8-20213170/src/main/java/com/example/lab820213170/Lab820213170Application.java
@@ -0,0 +1,13 @@
+package com.example.lab820213170;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820213170Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820213170Application.class, args);
+    }
+
+}
