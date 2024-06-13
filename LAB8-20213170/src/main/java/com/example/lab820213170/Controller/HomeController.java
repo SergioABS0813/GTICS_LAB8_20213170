2b5f12a9e52e8a5e5cd0689d6e39409569781bae
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-06-12 20:57:01 -0500

    update controller

--- LAB8-20213170/src/main/java/com/example/lab820213170/Controller/HomeController.java
+++ LAB8-20213170/src/main/java/com/example/lab820213170/Controller/HomeController.java
@@ -1,6 +1,31 @@
 package com.example.lab820213170.Controller;
 
+import com.example.lab820213170.Dao.PokemonDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
 public class HomeController {
+
+    final PokemonDao pokemonDao;
+
+    public ProductController(PokemonDao pokemonDao) {
+        this.pokemonDao = pokemonDao;
+    }
+
+    @GetMapping({"/list"})
+    public String listarProductos(Model model) {
+        //model.addAttribute("listaProductos", productRepository.findAll());
+        //Ahora lo sacamos del Dao y ya no del repositorio
+        model.addAttribute("listaProductos", productoDao.listar());
+        return "product/list";
+    }
+
+
 
 
 }
--- LAB8-20213170/src/main/java/com/example/lab820213170/Dao/PokemonDao.java
+++ LAB8-20213170/src/main/java/com/example/lab820213170/Dao/PokemonDao.java
@@ -1,9 +1,45 @@
 package com.example.lab820213170.Dao;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class PokemonDao {
+
+    public List<Product> listar() {
+
+        List<Product> lista = new ArrayList<>();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String endPoint = "http://localhost:8080/product";
+
+        //Parseo lo que extraigo en un arreglo de objetos
+        ResponseEntity<Product[]> responseEntity = restTemplate.getForEntity(endPoint, Product[].class);
+
+        //200 y algo siempre es succesfull para usar de solo un numero de codigo debe ser isSameCodeas
+
+        if(responseEntity.getStatusCode().is2xxSuccessful()){
+            Product[] body = responseEntity.getBody();
+            //Convertimos de arreglo a lista
+            lista = Arrays.asList(body);
+        }
+
+        return lista;
+    }
+
+
+
+
+
+
+
+
 
 
 
