2c62d4472a4021b3a04935868e3e5685a0b5a023
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-06-12 20:57:46 -0500

    Update PokemonDao.java

--- LAB8-20213170/src/main/java/com/example/lab820213170/Dao/PokemonDao.java
+++ LAB8-20213170/src/main/java/com/example/lab820213170/Dao/PokemonDao.java
@@ -10,28 +10,7 @@ import java.util.List;
 
 @Component
 public class PokemonDao {
-
-    public List<Product> listar() {
-
-        List<Product> lista = new ArrayList<>();
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        String endPoint = "http://localhost:8080/product";
-
-        //Parseo lo que extraigo en un arreglo de objetos
-        ResponseEntity<Product[]> responseEntity = restTemplate.getForEntity(endPoint, Product[].class);
-
-        //200 y algo siempre es succesfull para usar de solo un numero de codigo debe ser isSameCodeas
-
-        if(responseEntity.getStatusCode().is2xxSuccessful()){
-            Product[] body = responseEntity.getBody();
-            //Convertimos de arreglo a lista
-            lista = Arrays.asList(body);
-        }
-
-        return lista;
-    }
+    
 
 
 
